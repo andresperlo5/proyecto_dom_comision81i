203029dbebd60e741e9968253cd2726ca6911c3c
unknown <andresperlo5@gmail.com>
2023-10-11 11:14:21 -0300

    se crean admins

---
+++ js/usuarioAdmin.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        id: 1,
+        user: 'admin1',
+        pass: '123456789',
+        role: 'admin',
+        login: false,
+        deleted: false
+    },
+    {
+        id: 2,
+        user: 'admin2',
+        pass: '123456789',
+        role: 'admin',
+        login: false,
+        deleted: false
+    },
+]
+
+localStorage.setItem('users', JSON.stringify(users))
